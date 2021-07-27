5ef1d378c9f55d257a665113f0f2b0722597d8fe
kaisewhite <kaisewhite@kais-app.com>
2021-07-27 08:52:34 -0400

    checking for duplication

--- README.md
+++ README.md
@@ -9,5 +9,6 @@ then use the policy.put to replace the whole policy.
 This lambda function does the following:
 
 1. Checks for an existing policy
-2. If there is an existing policy it will append a new statement to the policy
-3. If there is not an existing policy it will create a new policy
+2. Checks for specific coniditions inside of the bucket policy
+3. If there is an existing policy it will append a new statement to the policy
+4. If there is not an existing policy it will create a new policy
--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def lambda_handler(event, context):
 
     for bucket in buckets:
 
-        # Create the bucket policy
+        # The bucket policy we want to append or create
         ssl_statement = {
             "Sid": "AllowSSLRequestsOnly",
             "Action": "s3:*",
@@ -24,21 +24,36 @@ def lambda_handler(event, context):
             "Principal": "*",
         }
         try:
+            # Get the existing bucket policy if it exists
             existing_policy: str = s3.get_bucket_policy(Bucket=bucket)
             statement: Dict[str, Any] = json.loads(existing_policy["Policy"])
             existing_statement = statement.get("Statement", [])
-            existing_statement.append(ssl_statement)
-            print(existing_statement)
-            new_policy = {
-                "Version": "2012-10-17",
-                "Id": "NewPolicy",
-                "Statement": existing_statement,
-            }
-            s3.put_bucket_policy(Bucket=bucket, Policy=json.dumps(new_policy))
+            # Check to see if the following condition already present in the existing policy
+            condition: str = "{\"Bool\": {\"aws:SecureTransport\": \"false\"}}"
+            for key_value in existing_statement:
+                print(json.dumps(key_value['Condition']))
+                if json.dumps(key_value['Condition']) == condition:
+                    print(
+                        f"This condition already exists in the following bucket: ${bucket}")
+                    print(f"Skipping bucket")
+                else:
+                    print(f"Creating bucket policy")
+                    new_policy = {
+                        "Version": "2012-10-17",
+                        "Id": "NewPolicy",
+                        "Statement": existing_statement,
+                    }
+                    s3.put_bucket_policy(
+                        Bucket=bucket, Policy=json.dumps(new_policy))
+                    print(f"Policy created for the following bucket: {bucket}")
         except s3.exceptions.from_code("NoSuchBucketPolicy"):
+            print(
+                f"No bucket policy exists for {bucket}")
+            print(f"Creating a new bucket policy")
             new_policy = {
                 "Version": "2012-10-17",
                 "Id": "EnforceSSLEncryption",
                 "Statement": ssl_statement,
             }
             s3.put_bucket_policy(Bucket=bucket, Policy=json.dumps(new_policy))
+            print(f"Policy created for the following bucket: {bucket}")
